c60b82f4b7040af7b0c784424b55d43c586c1306
Marcello C <marcello_ct@hotmail.com>
2026-06-20 00:48:07 -0300

    Project #2: Progress Steps

---
+++ 02_progress-steps/script.js
@@ -0,0 +1,57 @@
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const submit = document.getElementById("submit");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+
+  updateActiveStatus();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  updateActiveStatus();
+});
+
+function updateActiveStatus() {
+  circles.forEach((circle, i) => {
+    if (i < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+
+  //console.log(actives.length, circles.length);
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+    submit.disabled = false;
+    submit.textContent = "Submit";
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+    submit.disabled = true;
+    submit.textContent = "Make all Steps";
+  }
+}
